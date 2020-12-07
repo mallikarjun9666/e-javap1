b215458aada3f48619eb0ad6d730e821b8f4e223
arjun <arjun123@gmail.com>
2020-12-07 23:38:18 +0530

    commit 1

---
+++ src/test/java/ejavap1/factorialtest.java
@@ -0,0 +1,14 @@
+package ejavap1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class factorialtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
